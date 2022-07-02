7773b764440e64757873403038220b486dcc004b
YahorLukyanchyk <108582464+YahorLukyanchyk@users.noreply.github.com>
2022-07-02 18:19:01 +0300

    Update spaceRemoval.js

--- scripts/spaceRemoval.js
+++ scripts/spaceRemoval.js
@@ -1,5 +1,3 @@
-const emptyValue = '';
-
 function spaceRemoval(){
     let inputData = document.getElementById('textarea1').value;
     let outputData = inputData.replaceAll(' ', '');
@@ -7,15 +5,6 @@ function spaceRemoval(){
     textarea2.value = outputData;
 }
 
-function clearValues(){
-    let textarea1 = document.getElementById('textarea1');
-    let textarea2 = document.getElementById('textarea2');
-    textarea1.value = emptyValue;
-    textarea2.value = emptyValue;
-}
-
-
-
 document.querySelector('.run').addEventListener('click', spaceRemoval);
-document.querySelector('.clear').addEventListener('click', clearValues);
+
 console.log('Space Removal Script Is Loaded!');
